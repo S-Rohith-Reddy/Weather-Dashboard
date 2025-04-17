5757969ca549b3690abc10dd107aa726a6e0f8a0
S.Rohith Reddy <sareddyrohith@gmail.com>
2025-04-17 07:32:30 +0530

    Fix for Render: Updated host and port

--- weather_app.py
+++ weather_app.py
@@ -51,5 +51,8 @@ def home():
         weather = get_weather_data(city)
     return render_template("index.html", weather=weather)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
